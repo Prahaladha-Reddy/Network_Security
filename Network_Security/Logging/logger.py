80b689431ed3175b07daf1382a208f03098818cd
Prahaladha-Reddy <prahaladreddy80@gmail.com>
2024-12-01 12:54:00 +0530

    Add mongo push and push to mongo

--- Network_Security/Logging/logger.py
+++ Network_Security/Logging/logger.py
@@ -2,22 +2,15 @@ import logging
 import os
 from datetime import datetime
 
-# Construct the log file path relative to the "Network_Security" folder
-log_folder = os.path.join(os.path.dirname(__file__), "logs")
-if not os.path.exists(log_folder):
-    os.makedirs(log_folder)
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-logs_path = os.path.join(log_folder, "network_security.log")
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
 
-LOG_FILE = f"[{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}].log"
-logs_path = os.path.join(logs_path, LOG_FILE)
-
-os.makedirs(os.path.dirname(logs_path), exist_ok=True)
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
-    filename=logs_path,
-    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s: %(message)s",
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
-
-logger = logging.getLogger(__name__)
