26cc69c8138c61eed4bb593c915137e34c8d9b15
Prahaladha-Reddy <prahaladreddy80@gmail.com>
2024-12-04 15:40:59 +0530

    Resolved the bugs

--- Network_Security/Components/data_ingestion.py
+++ Network_Security/Components/data_ingestion.py
@@ -18,6 +18,7 @@ load_dotenv()
 MONGO_DB_URL=os.getenv("MONGO_DB_URL")
 
 
+
 class DataIngestion:
     def __init__(self,data_ingestion_config:DataIngestionConfig):
         try:
@@ -42,7 +43,7 @@ class DataIngestion:
             df.replace({"na":np.nan},inplace=True)
             return df
         except Exception as e:
-            raise NetworkSecurityException
+            raise NetworkSecurityException(e,sys)
         
     def export_data_into_feature_store(self,dataframe: pd.DataFrame):
         try:
@@ -66,7 +67,7 @@ class DataIngestion:
             logging.info(
                 "Exited split_data_as_train_test method of Data_Ingestion class"
             )
-           
+            
             dir_path = os.path.dirname(self.data_ingestion_config.training_file_path)
             
             os.makedirs(dir_path, exist_ok=True)
@@ -93,7 +94,7 @@ class DataIngestion:
             dataframe=self.export_data_into_feature_store(dataframe)
             self.split_data_as_train_test(dataframe)
             dataingestionartifact=DataIngestionArtifact(trained_file_path=self.data_ingestion_config.training_file_path,
-            test_file_path=self.data_ingestion_config.testing_file_path)
+                                                        test_file_path=self.data_ingestion_config.testing_file_path)
             return dataingestionartifact
 
         except Exception as e:
--- main.py
+++ main.py
@@ -1,18 +1,25 @@
+import os
 from Network_Security.Components.data_ingestion import DataIngestion
 from Network_Security.Exeptions_Handle.exeption import NetworkSecurityException
 from Network_Security.Logging.logger import logging
-from Network_Security.Entity.config_entity import DataIngestionConfig
-
-from Network_Security.Entity.config_entity import TrainingPipelineConfig
+from Network_Security.Entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
 import sys
-if __name__=='__main__':
+
+if __name__ == '__main__':
     try:
-        trainingpipelineconfig=TrainingPipelineConfig()
-        dataingestionconfig=DataIngestionConfig(trainingpipelineconfig)
-        data_ingestion=DataIngestion(dataingestionconfig)
-        logging.info("Initiate the data ingestion")
-        dataingestionartifact=data_ingestion.initiate_data_ingestion()
-        logging.info("Data Initiation Completed")
-        print(dataingestionartifact)
+        training_pipeline_config = TrainingPipelineConfig()
+        
+        # Ensure the artifact directory is created
+        if not os.path.exists(training_pipeline_config.artifact_dir):
+            os.makedirs(training_pipeline_config.artifact_dir)
+            logging.info(f"Created artifact directory at {training_pipeline_config.artifact_dir}")
+        
+            trainingpipelineconfig=TrainingPipelineConfig()
+            dataingestionconfig=DataIngestionConfig(trainingpipelineconfig)
+            data_ingestion=DataIngestion(dataingestionconfig)
+            logging.info("Initiate the data ingestion")
+            dataingestionartifact=data_ingestion.initiate_data_ingestion()
+            logging.info("Data Initiation Completed")
+            print(dataingestionartifact)
     except Exception as e:
-        NetworkSecurityException(e,sys)
+           raise NetworkSecurityException(e,sys)
